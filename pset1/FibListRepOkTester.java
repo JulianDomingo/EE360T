f45ede7484c33b4154580525b39b225aacf28597
Julian Domingo <julian@wireless-10-146-4-109.public.utexas.edu>
2017-02-07 13:17:36 -0600

    Finished test suite for repOk()

--- pset1/FibListRepOkTester.java
+++ pset1/FibListRepOkTester.java
@@ -6,24 +6,44 @@ import org.junit.Test;
 public class FibListRepOkTester {
 	// INVALIDITY TESTS
 	@Test public void t0() {
-		FibList fibList = new FibList();
-		
+
 		// Test invalid sizes
+		FibList fibList = new FibList();
 		for (int newSize = 0; newSize < 3; newSize++) {
 			fibList.size = newSize;
 			assertFalse(fibList.repOk());
 		}
 
+		// Test fibonacci invalidity
+		fibList = new FibList();
+		fibList.header = new Node();
+		fibList.header.elem = 1;
+		fibList.header.next = new Node();
+		fibList.header.next.elem = 1;
+		fibList.header.next.next = new Node();
+		fibList.header.next.next.elem = 3;
+
+		assertFalse(fibList.repOk());
+
 		// Test acyclic list
 		fibList = new FibList();
+		Node headerCopy = fibList.header;
+		Node runner = fibList.header;
 
+		while (runner.next != null) {
+			runner = runner.next;
+		}
 
+		runner.next = headerCopy;
+
+		assertFalse(fibList.repOk());
 	}
+
 
 	// VALIDITY TESTS
 	@Test public void t1() {
 		FibList fibList = new FibList();
-		
+
 		// Test fibonacci validity
 		fibList = new FibList();
 		assertTrue(fibList.header.elem == 1);
@@ -40,8 +60,18 @@ public class FibListRepOkTester {
 			runner = runner.next;
 		}
 
-		// Confirm not acyclic
+		// Test size validity
+		fibList = new FibList();
+		int size = 0;
 
+		Node runner = fibList.header;
+
+		while (runner.next != null) {
+			size++;
+			runner = runner.next;
+		}
+
+		assertTrue(size == fibList.size);
 	}
 
 }
