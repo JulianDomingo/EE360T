c392d4520ea0acb530ed85ad5b9e9c5edd008105
Julian Domingo <julian.domingo@utexas.edu>
2017-03-09 19:28:09 -0600

    Finished implementation of methods in class D

--- pset2/C.java
+++ pset2/C.java
@@ -28,30 +28,3 @@ public class C {
         return hash;        
     } 
 }
-
-public class D extends C {
-    int g;
-
-    public D(String s, int g) {
-        super(s);
-        this.g = g;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        // returns a boolean consistent with the Java contract for equals method
-        // returns true if and only if o is an object of class D and
-        // has the same value for field s as this.s (w.r.t. String equals) and
-        // the same value for field g as this.g
-        //
-        // your code goes here
-    }
-
-    @Override
-    public int hashCode() {
-        // returns an integer consistent with the Java contract for hashCode method;
-        // does not return a constant value
-        //
-        //
-    }
-}
---
+++ pset2/D.java
@@ -0,0 +1,36 @@
+/*
+ * Julian Domingo : jad5348
+ */
+
+package pset2;
+
+public class D extends C {
+    int g;
+
+    public D(String s, int g) {
+        super(s);
+        this.g = g;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o instanceof D &&
+           (this.s.equals(((D) o).s) &&
+           (this.g == ((D) o).g)))
+        {
+            return true;            
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 1;
+        hash = hash * 10 + g;
+        hash = hash * 15 + (s == null ? 0 : s.hashCode());
+        return hash;
+    }
+}
