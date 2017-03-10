29187a9621f9638eea9beb9363b1fe12fccf88fa
Julian Domingo <julian.domingo@utexas.edu>
2017-03-10 10:15:30 -0600

    Finished test methods for the 4 properties

--- pset2/EqualsTester.java
+++ pset2/EqualsTester.java
@@ -13,16 +13,33 @@ public class EqualsTester {
     }
 
     /*
-     * P4: It is transitive: for any non-null reference values x, y, and z,
+     * P2: Reflexive: For any non-null reference value x, x.equals(x) should return true.
+     */
+    @Test public void t1() {
+        C c = new C("Hello.");
+        assertTrue(c.equals(c));
+    }
+
+    /*
+     * P3: Symmetric: For any non-null reference values x and y, x.equals(y) should return true if and only if y.equals(x) returns true.
+     */
+    @Test public void t2() {
+        D d1 = new D("Salutations.", 10);
+        D d2 = new D("Salutations.", 10);
+        assertTrue(d1.equals(d2) && d2.equals(d1));
+    }
+
+    /*
+     * P4: Transitive: For any non-null reference values x, y, and z,
      * if x.equals(y) returns true and y.equals(z) returns true, then
      * x.equals(z) should return true.
      */
-    @Test public void t1() {
+    @Test public void t3() {
+        Object x = new Object();
+        Object y = new Object();
+        Object z = new Object();
 
-    }
-
-    @Test public void t2() {
-
+        assertTrue(x.equals(y) && y.equals(z) == x.equals(z));
     }
 }
 
