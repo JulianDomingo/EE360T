5b346861e85378f0d81fbc609a4f67cc9189058f
Julian Domingo <julian.domingo@utexas.edu>
2017-04-03 18:47:19 -0500

    Finished addEdge() implementation

--- pset4/Graph.java
+++ pset4/Graph.java
@@ -6,6 +6,8 @@
 
 import java.util.*;
 
+// Class Invariant: Fields "nodes" and "edges" are non-null;
+//                  "this" graph has no node that is not in "nodes"
 public class Graph {
     private Set<Integer> nodes;
     private Map<Integer, List<Integer>> edges;
@@ -24,13 +26,19 @@ public class Graph {
     }
 
     public void addEdge(int from, int to) {
-        
+        if (!edges.containsKey(from)) {
+            edges.put(from, new ArrayList<Integer>(Arrays.asList(to)));
+        }
+        else if (!edges.get(from).contains(to)) {
+            edges.get(from).add(to);;
+        }
     }
 
     public boolean unreachable(Set<Integer> sources, Set<Integer> targets) {
         if (sources == null || targets == null) {
             throw new IllegalArgumentException();
         }
+    
     }
 
 
